fae9e01a65858f54147bbe91fff99a237c360001
Nate <Natesky9@gmail.com>
2025-02-17 20:46:09 -0500

    changed plugin file name to lowercase

--- src/test/java/com/natesky9/vitality.java
+++ src/test/java/com/natesky9/vitality.java
@@ -3,7 +3,7 @@ package com.natesky9;
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class Vitality
+public class vitality
 {
 	public static void main(String[] args) throws Exception
 	{
